ccff617c8d48284441b967828dfeacffbded357a
zazzaze <egor.anikeev.00@gmail.com>
2021-04-19 23:29:33 +0300

    fix

--- 09-3.c
+++ 09-3.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/sem.h>
+#include <errno.h>
 
 int increment(int semid, struct sembuf* buf) {
 	buf->sem_op = 1;
@@ -28,56 +29,64 @@ int main() {
 	char pathname[] = "09-3.c";
 	struct sembuf buffer;
 	char result_string[14];
-	int semid;	
+	int semid;
+	
+	int N;
+	printf("Write N: ");
+	scanf("%d", &N);			
+
 	//create pipe
 	if (pipe(pch) < 0) {
 		printf("Pipe error\n");
 		exit(-1);
 	}
 	
-	if ((semid = semget(key, 1, 0666)) < 0) {
-		printf("Semaphore does does not exists. Create...\n");
-		if((semid = semget(key, 1, 0666 | IPC_CREAT) < 0)) {
-			printf("Semaphore creation error");
+	if ((semid = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
+		if (errno != EEXIST) {
+			printf("Creation error\n");
 			exit(-1);
 		}
-		printf("Semaphore created");
+		if((semid = semget(key, 1, 0666 | IPC_CREAT) < 0)) {
+			printf("Semaphore find error");
+			exit(-1);
+		}
 	}
-
 
 	if ((res = fork()) < 0) {
 		printf("Fork error\n");
 		exit(-1);
 	} else if (res > 0) {
 		//parent process
-		int N;
-		printf("Write N ");
-		scanf("%d", &N);
 		for (size_t i = 0; i < N; ++i) {
 			if (write(pch[1], "Hello, world!", 14) != 14) {
 				printf("Parent message write error!");
 				exit(-1);
 			}
 			
-			//child can read and write
-			increment(semid, &buffer);
-			//parent wait
-			decrement(semid, &buffer);
+			//parent wait - -1
+			if (decrement(semid, &buffer) < 0) {
+				printf("Sem cant wait\n");
+				exit(-1);
+			}
+			
 			if ((size = read(pch[0], result_string, 14) != 14)) {
 				printf("Parent read from child error!\n");
 				exit(-1);
 			}
 			printf("Parent read from child %s\n", result_string);
 		}
-		close(pch[0]);
+		if (close(pch[0]) < 0) {
+			printf("Cant close pipe\n");
+			exit(-1);
+		}
+		if (close(pch[1]) < 0) {
+			printf("Cant close pipe\n");
+			exit(-1);
+		}
 	} else {
 		//child process
-		while (1) {
-			//wait parent
-			decrement(semid, &buffer);
+		for (int i = 0; i < N; ++i) {
 			if ((size = read(pch[0], result_string, 14)) < 0) {
-				close(pch[1]);
-				close(pch[0]);
 				printf("This is the end\n");
 				return 0;
 			}
@@ -86,8 +95,20 @@ int main() {
 				printf("Child write error\n");
 				exit(-1);
 			}
-			//parent access
-			increment(semid, &buffer);
+			//parent access - increment semahpore
+			if (increment(semid, &buffer) < 0) {
+				printf("Cant\'t inc sem\n");
+				exit(-1);
+			}
+		}
+		
+		if (close(pch[0]) < 0) {
+			printf("Cant close pipe\n");
+			exit(-1);
+		}
+		if (close(pch[1]) < 0) {
+			printf("Cant close pipe\n");
+			exit(-1);
 		}
 	}
 }
