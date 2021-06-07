cb35949bae8468857ac3b974592f02f2d6add98c
zazzaze <egor.anikeev.00@gmail.com>
2021-06-07 14:55:42 +0300

    done

---
+++ 14-4receiver.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int value;
+int byte;
+
+void zero(int nsig) {
+  byte <<= 1;
+}
+
+void one(int nsig) {
+  value |= byte;
+  byte <<= 1;
+}
+
+int main(void) {
+  pid_t my_pid, sender_pid;
+  value = 0;
+  byte = 1;
+  (void) signal(SIGUSR1, zero);
+  (void) signal(SIGUSR2, one);
+
+  my_pid = getpid();
+  printf("Receiver pid = %d\n", my_pid);
+
+  printf("Sender pid = : ");
+  scanf("%d", &sender_pid);
+
+  while (1) {
+    if(byte == 0) {
+      printf("Received = %d\n", value);
+      break;
+    }
+  }
+}
---
+++ 14-4sender.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void) {
+  pid_t my_pid, to_pid;
+  int value;
+
+  my_pid = getpid();
+  printf("Sender pid = %d\n", my_pid);
+
+  printf("Enter pid = ");
+  scanf("%d", &to_pid);
+
+  printf("Enter value = ");
+  scanf("%d", &value);
+
+  int byte = 1;
+  while (byte != 0) {
+    if ((value & byte) == 0) {
+      kill(to_pid, SIGUSR1);
+    } else {
+      kill(to_pid, SIGUSR2);
+    }
+    byte = byte << 1;
+	for (int i = 0; i < 100000000; ++i);
+  }
+}
